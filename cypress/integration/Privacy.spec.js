14a8b352e652a7da49517cfb1d5d73fdfb02e40a
Leonardo Santana <leonardosantana_in@hotmail.com>
2024-08-21 16:00:01 -0300

    new changes until lesson 9

--- cypress/integration/Privacy.spec.js
+++ cypress/integration/Privacy.spec.js
@@ -3,7 +3,7 @@ describe('Politica de privacidade TAT', () => {
     beforeEach(() => {
         cy.visit("./src/privacy.html")
     })
-    it.only('testa a página da política de privacidade de forma independente', () => {
+    it('testa a página da política de privacidade de forma independente', () => {
 
         cy.url().should('include', 'privacy.html')
         cy.contains('Talking About Testing')
